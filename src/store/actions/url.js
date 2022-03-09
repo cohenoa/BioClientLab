57915ca98cbd6dd144a848976aad0f5a9315373a
doriuran <doryuran090@gmail,com>
2022-03-09 19:04:12 +0200

    Url

--- src/store/actions/url.js
+++ src/store/actions/url.js
@@ -1,4 +1,4 @@
-const URL = 'http://10.8.6.223:8080/'
+const URL = 'https://bioserverlab.herokuapp.com/'
 // NEW PATH'https://bioserverlab.herokuapp.com/'
 // OLD PATHhttps://bioinformaticswebsite.herokuapp.com/
 // http://192.168.1.45:8080
